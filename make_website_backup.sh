583a031dff9082f7adee310842e0f6d9f9b61eac
Ignas <13539780+ignasposka@users.noreply.github.com>
2018-05-28 19:31:19 +0300

    Implemented backups directory creation feature.

--- make_website_backup.sh
+++ make_website_backup.sh
@@ -1,14 +1,15 @@
-
 #!/bin/sh
 backups_capacity=10
-if [ ! -d "/root/websites_backup/$1" ]; then
+if [ ! -d "/root/websites_backup" ]; then
+mkdir /root/websites_backup
+mkdir /root/websites_backup/$1
+elif [ ! -d "/root/websites_backup/$1" ]; then
   mkdir /root/websites_backup/$1
 fi
 DATE=`date '+%Y-%m-%d_%H:%M:%S'`
-mkdir /root/websites_backup/$DATE
 cp -r /var/www/$1 /root/websites_backup/$1/$DATE
 
-number_to_delete=$((`ls /root/websites_backup/$1 | wc -l` - $backups_capacity$
+number_to_delete=$((`ls /root/websites_backup/$1 | wc -l` - $backups_capacity
 if [ "$number_to_delete" -gt 0 ]; then
 cd /root/websites_backup/$1
 ls -t /root/websites_backup/$1 | tail -n $number_to_delete | xargs -d '\n' r$

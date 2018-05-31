4d4b16472b42a2e3964381683ae69ba3fab22f43
Ignas <Ignasp789@gmail.com>
2018-05-31 14:51:52 +0300

    Fixed mistake

--- make_website_backup.sh
+++ make_website_backup.sh
@@ -9,7 +9,7 @@ fi
 DATE=`date '+%Y-%m-%d_%H:%M:%S'`
 cp -r /var/www/$1 /root/websites_backup/$1/$DATE
 
-number_to_delete=$((`ls /root/websites_backup/$1 | wc -l` - $backups_capacity
+number_to_delete=$((`ls /root/websites_backup/$1 | wc -l` - $backups_capacity))
 if [ "$number_to_delete" -gt 0 ]; then
 cd /root/websites_backup/$1
 ls -t /root/websites_backup/$1 | tail -n $number_to_delete | xargs -d '\n' r$
